2712888ad863abb9174c923174ef405bde93a548
Ausitn Danaj <afdanaj@oakland.edu>
2017-03-13 15:20:42 -0400

    Added blank sql statement

--- CSE345Website/CSE345Website/SignIn.aspx.cs
+++ CSE345Website/CSE345Website/SignIn.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace CSE345Website
 {
@@ -15,6 +16,21 @@ namespace CSE345Website
         }
         protected void SignIn_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = "Data Source=localhost;Initial Catalog=afdanaj;User id=afdanaj;Password=temp12345;";
+                conn.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SELECT * FROM CSE345_Students;";
+                command.Connection = conn;
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch( Exception ex)
+            {
+
+            }
 
         }
     }
